1102fdb50e6b143015178084417f5a67fa6f09c2
Miloš Čuturić <miloscuturic8@gmail.com>
2026-02-17 13:26:26 +0100

    fix: Utility scripts and docker-compose

--- utility-scripts/build-all.sh
+++ utility-scripts/build-all.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 
-services="user-service accommodation-service notification-service rating-service reservation-service search-service gateway-service"
+services="user-service accommodation-service notification-service rating-service reservation-service gateway-service"
 
 cd ../../
 for service in $services; do
   echo "Building $service ..."
-  cd "devoops-$service" && chmod +x gradlew && ./gradlew clean build
+  cd "devoops-$service" && chmod +x gradlew && ./gradlew clean build -x test
   echo "Done building $service"
   cd ..
 done
+
+echo "Building images..."
+cd devoops-core
+docker compose up -d --build
+echo "Done building images"
